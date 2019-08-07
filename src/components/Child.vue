ac0186e2f709517afd3f0147d0c1a27a4fb6ada3
zxf <2335741644@qq.com>
2019-08-07 10:07:01 +0800

    封装$bus

---
+++ src/components/Child.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        我是一个子组件
+    </div>
+</template>
+<script>
+export default {
+    props:{
+
+    },
+    components:{
+
+    },
+    data(){
+        return {
+
+        }
+    },
+    computed:{
+
+    },
+    methods:{
+
+    },
+    created(){
+
+    },
+    mounted(){
+
+    }
+}
+</script>
+<style scoped lang="">
+
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <!-- <input type="text" v-model="modelInp"> -->
+    <!-- <VModel :value="modelInp"></VModel> -->
+    <VModel @input="ParentFun"></VModel>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      modelInp:"定义的属性"
+    };
+  },
+  components: {
+    VModel: {
+      props: ["value"],
+      template: `<div>
+                    子组件{{value}}
+                    <button @click="ChangeVal">修改</button>
+                  </div>`,
+      methods:{
+        ChangeVal(){
+          console.log(1)
+          // this.$emit('input','改一改属性')
+         }
+      }
+    }
+  },
+  methods:{
+    ParentFun(){
+      // console.log(1)
+    }
+  }
+}
+</script>
+<style scoped lang="">
+</style>
